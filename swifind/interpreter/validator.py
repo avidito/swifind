c63a267b17a31a6a17183f14bd77adf130d29c72
leonardi <av.leonardif@gmail.com>
2021-04-04 22:23:42 +0700

    add exception for validate_collect

--- swifind/interpreter/validator.py
+++ swifind/interpreter/validator.py
@@ -10,8 +10,11 @@ def validate_root(component, line_id):
     args = re.findall(r"([^'\s]\S*|'.+?')", component)
     arguments_error = ArgumentsError('root', line_id)
     args_count = len(args)
+
+    # Argument Count
     if (args_count < 1): arguments_error.missing(argument_missing=ARGUMENTS['root'][args_count])
     elif(args_count > 1): arguments_error.over(argument_need=1, argument_given=args_count)
+    # Argument Type
     elif(not args[0].isprintable()): arguments_error.type(argument_type_error=ARGUMENTS['root'][0])
     else: return
 
@@ -22,13 +25,20 @@ def validate_collect(component, line_id):
     Validate syntax of collect activity
     """
     args = re.findall(r"([^'\s]\S*|'.+?')", component)
-    c = [
-        len(args) == 2,
-        args[0].isalnum(),
-        args[1].isprintable()
-    ]
-    return all(c)
+    arguments_error = ArgumentsError('collect', line_id)
+    args_count = len(args)
 
+    # Argument Count
+    if (args_count < 2): arguments_error.missing(argument_missing=ARGUMENTS['collect'][args_count])
+    elif(args_count > 2): arguments_error.over(argument_need=2, argument_given=args_count)
+    # Argument Type
+    elif(not args[0].isalnum()): arguments_error.type(argument_type_error=ARGUMENTS['collect'][0])
+    elif(not args[1].isprintable()): arguments_error.type(argument_type_error=ARGUMENTS['collect'][1])
+    else: return
+
+    raise arguments_error
+
+# Mapper
 VALIDATORS = {
     'root': validate_root,
     'collect': validate_collect
