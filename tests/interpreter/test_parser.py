d87ba785a6c329b8892f1a91c3bfa8149dc13655
Leonardi Fabianto <av.leonardif@gmail.com>
2021-04-17 07:44:30 +0700

    add test for parse_swipl

--- tests/interpreter/test_parser.py
+++ tests/interpreter/test_parser.py
@@ -2,7 +2,7 @@ import os
 import types
 import pytest
 
-from tests.constant import READ_SCRIPT_PATH
+from tests.constant import READ_SCRIPT_PATH, PARSE_SWIPL_PATH
 
 from swifind.interpreter.parser import (read_script,
                                         parse_swipl)
@@ -12,10 +12,17 @@ class TestReadScript(object):
         path = os.path.join(READ_SCRIPT_PATH, 'valid_components_ex1.swipl')
         raw_components = read_script(path)
         assert isinstance(raw_components, types.GeneratorType)
-        for component in raw_components:
+
+        expected = [
+                (1, "ORIGIN https://www.testing.com/"),
+                (3, "PICK title 'h1 a text'"),
+            ]
+        for component, exp in zip(raw_components, expected):
             assert isinstance(component, tuple)
             assert isinstance(component[0], int)
             assert isinstance(component[1], str)
+            assert component == exp
+
 
     def test_with_empty_script(self):
         path = os.path.join(READ_SCRIPT_PATH, 'empty_script.swipl')
@@ -27,7 +34,30 @@ class TestReadScript(object):
 
 class TestParseSwipl(object):
     def test_with_valid_script(self):
-        ... # Implement later
+        path = os.path.join(PARSE_SWIPL_PATH, 'valid_script_ex1.swipl')
+        components = parse_swipl(path)
+        assert isinstance(components, types.GeneratorType)
+
+        expected = [
+                (1, 'ORIGIN', 'http://quotes.toscrape.com'),
+                (2, 'PICK', "title 'h1 a text'"),
+                (3, 'PICK', "subtitle 'h2 a text'")
+            ]
+        for component, exp in zip(components, expected):
+            assert component == exp
 
     def test_with_no_argument_activity(self):
-        ... # Implement later
+        path = os.path.join(PARSE_SWIPL_PATH, 'no_argument_activity_ex1.swipl')
+        components = parse_swipl(path)
+        assert isinstance(components, types.GeneratorType)
+
+        expected = [
+                (1, 'ORIGIN', ''),
+                (2, 'PICK', '')
+            ]
+        for component, exp in zip(components, expected):
+            assert isinstance(component, tuple)
+            assert isinstance(component[0], int)
+            assert isinstance(component[1], str)
+            assert isinstance(component[2], str)
+            assert component == exp
