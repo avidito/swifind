8e73eac669796b86a21868369a4e076be187a698
Leonardi Fabianto <av.leonardif@gmail.com>
2021-04-17 08:30:29 +0700

    add test for catfish initiation and prepare

--- tests/constant.py
+++ tests/constant.py
@@ -3,6 +3,7 @@ import os
 TEST_PATH = os.path.dirname(__file__)
 DUMMY_PATH = os.path.join(TEST_PATH, '_dummy')
 
+CATFISH_PREPARE = os.path.join(DUMMY_PATH, 'test_validate_swipl')
 READ_SCRIPT_PATH = os.path.join(DUMMY_PATH, 'test_read_script')
 PARSE_SWIPL_PATH = os.path.join(DUMMY_PATH, 'test_parse_swipl')
 VALIDATE_SWIPL_PATH = os.path.join(DUMMY_PATH, 'test_validate_swipl')
